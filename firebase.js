b868af0d6038a8dad427d6465f8e6cf61a6c9dda
Anuj1-web <singa6927@gmail.com>
2025-07-31 09:51:46 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,15 +1,19 @@
 // firebase.js
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.22.0/firebase-app.js';
 import { getAuth } from 'https://www.gstatic.com/firebasejs/9.22.0/firebase-auth.js';
+import { getFirestore } from 'https://www.gstatic.com/firebasejs/9.22.0/firebase-firestore.js';
+import { getStorage } from 'https://www.gstatic.com/firebasejs/9.22.0/firebase-storage.js';
 
 const firebaseConfig = {
   apiKey: "AIzaSyCpoq_sjH_XLdJ1ZRc0ECFaglvXh3FIS5Q",
   authDomain: "the-little-info.firebaseapp.com",
   projectId: "the-little-info",
-  storageBucket: "the-little-info.firebasestorage.app",
+  storageBucket: "the-little-info.appspot.com",
   messagingSenderId: "165711417682",
   appId: "1:165711417682:web:cebb205d7d5c1f18802a8b"
 };
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
