afdadbeea6eb447e7d6fee0d010e0435676d4539
Anuj1-web <singa6927@gmail.com>
2025-08-04 17:45:46 +0530

    Update traditional.js

--- traditional.js
+++ traditional.js
@@ -12,7 +12,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyCpoq_sjH_XLdJ1ZRc0ECFaglvXh3FIS5Q",
   authDomain: "the-little-info.firebaseapp.com",
   projectId: "the-little-info",
-  storageBucket: "the-little-info.firebasestorage.app",
+  storageBucket: "the-little-info.appspot.com", // ✅ FIXED
   messagingSenderId: "165711417682",
   appId: "1:165711417682:web:cebb205d7d5c1f18802a8b"
 };
