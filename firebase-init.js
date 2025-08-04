c6cfe1094ec93a4e60a553ea1e63804f6d844e15
Anuj1-web <singa6927@gmail.com>
2025-08-04 18:47:49 +0530

    Update firebase-init.js

--- firebase-init.js
+++ firebase-init.js
@@ -1,9 +1,4 @@
 // firebase-init.js
-
-// Make sure Firebase SDKs are loaded in HTML before this file:
-// <script src="https://www.gstatic.com/firebasejs/9.6.1/firebase-app-compat.js"></script>
-// <script src="https://www.gstatic.com/firebasejs/9.6.1/firebase-auth-compat.js"></script>
-
 const firebaseConfig = {
   apiKey: "AIzaSyCpoq_sjH_XLdJ1ZRc0ECFaglvXh3FIS5Q",
   authDomain: "the-little-info.firebaseapp.com",
@@ -13,5 +8,4 @@ const firebaseConfig = {
   appId: "1:165711417682:web:cebb205d7d5c1f18802a8b"
 };
 
-// Initialize Firebase
 firebase.initializeApp(firebaseConfig);
