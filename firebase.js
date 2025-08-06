4f6c2fbb882864505c7348691d2bdf07b1fac3ab
Anuj1-web <singa6927@gmail.com>
2025-08-06 19:48:12 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,25 +1,18 @@
 // firebase.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
-
-// ✅ Named export for config (so other files can import it)
-export const firebaseConfig = {
+const firebaseConfig = {
   apiKey: "AIzaSyCpoq_sjH_XLdJ1ZRc0ECFaglvXh3FIS5Q",
   authDomain: "the-little-info.firebaseapp.com",
   projectId: "the-little-info",
-  storageBucket: "the-little-info.appspot.com",
+  storageBucket: "the-little-info.appspot.com",  // ✅ FIXED typo here
   messagingSenderId: "165711417682",
-  appId: "1:165711417682:web:cebb205d7d5c1f18802a8b"
+  appId: "1:165711417682:web:cebb205d7d5c1f18802a8b",
+  measurementId: "G-8KTFTYZBSL"
 };
 
-// Initialize Firebase app
 const app = initializeApp(firebaseConfig);
-
-// Initialize services
-const db = getFirestore(app);
-const auth = getAuth(app);
-
-// ✅ Export services for use in other files
-export { db, auth };
+export const db = getFirestore(app);
+export const auth = getAuth(app);
