2c5acce729ce7594949cd0650ee519aaaa5518cf
Anuj1-web <singa6927@gmail.com>
2025-08-06 21:04:00 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,18 +1,15 @@
 // firebase.js
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.22.0/firebase-app.js';
+import { getFirestore } from 'https://www.gstatic.com/firebasejs/9.22.0/firebase-firestore.js';
 
 const firebaseConfig = {
   apiKey: "AIzaSyCpoq_sjH_XLdJ1ZRc0ECFaglvXh3FIS5Q",
   authDomain: "the-little-info.firebaseapp.com",
   projectId: "the-little-info",
-  storageBucket: "the-little-info.appspot.com",  // ✅ FIXED typo here
+  storageBucket: "the-little-info.appspot.com",
   messagingSenderId: "165711417682",
-  appId: "1:165711417682:web:cebb205d7d5c1f18802a8b",
-  measurementId: "G-8KTFTYZBSL"
+  appId: "1:165711417682:web:cebb205d7d5c1f18802a8b"
 };
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
-export const auth = getAuth(app);
