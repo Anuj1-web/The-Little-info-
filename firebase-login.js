6a2de5ea5ff3314458cb21df1bc21e98854a1b49
Anuj1-web <singa6927@gmail.com>
2025-08-07 10:11:22 +0530

    Update firebase-login.js

--- firebase-login.js
+++ firebase-login.js
@@ -5,13 +5,15 @@ import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-aut
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
 // 🔐 This config can be same or separate depending on your auth rules
-const firebaseLoginConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "your-app-id.firebaseapp.com",
-  projectId: "your-app-id",
-  storageBucket: "your-app-id.appspot.com",
-  messagingSenderId: "123456789",
-  appId: "APP_ID"
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCpoq_sjH_XLdJ1ZRc0ECFaglvXh3FIS5Q",
+  authDomain: "the-little-info.firebaseapp.com",
+  projectId: "the-little-info",
+  storageBucket: "the-little-info.firebasestorage.app",
+  messagingSenderId: "165711417682",
+  appId: "1:165711417682:web:cebb205d7d5c1f18802a8b",
+  measurementId: "G-8KTFTYZBSL"
 };
 
 const loginApp = initializeApp(firebaseLoginConfig, "loginApp"); // named app instance
